110952808027fa6cdaa5494f1503357c9bd0c270
drb0r1s <drb0r1sdev@gmail.com>
2026-07-17 12:25:50 +0200

    doktorc.rs run function update

--- src/bin/doktorc.rs
+++ src/bin/doktorc.rs
@@ -4,6 +4,17 @@ use std::env;
 use std::fs;
 use std::process;
 
+fn run(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let source = fs::read_to_string(path)?;
+
+    let tokens = Tokenizer::new(&source).tokenize()?;
+    let doktor_node = Parser::new(tokens).parse()?;
+
+    println!("{}", doktor_node);
+    
+    Ok(())
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -12,37 +23,8 @@ fn main() {
         process::exit(1);
     }
 
-    let path: &str = &args[1];
-
-    let source: String = match fs::read_to_string(path) {
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!("[DOKTOR: Compiler] Could not open file '{}': {}", path, e);
-            process::exit(1);
-        }
-    };
-
-    let tokenizer: Tokenizer = Tokenizer::new(&source);
-
-    match tokenizer.tokenize() {
-        Ok(tokens) => {
-            let parser: Parser = Parser::new(tokens);
-
-            match parser.parse() {
-                Ok(doktor_node) => {
-                    println!("{}", doktor_node);
-                }
-
-                Err(e) => {
-                    eprintln!("{}", e);
-                    process::exit(1);
-                }
-            }
-        }
-
-        Err(e) => {
-            eprintln!("{}", e);
-            process::exit(1);
-        }
+    if let Err(e) = run(&args[1]) {
+        eprintln!("[DOKTOR: Compiler] {}", e);
+        process::exit(1);
     }
 }
