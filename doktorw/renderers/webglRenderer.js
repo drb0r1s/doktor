bf328fec8ff3eb2cdae212f179dbaae826b9f791
drb0r1s <drb0r1sdev@gmail.com>
2026-07-25 13:48:06 +0200

    doktorw: WebglRenderer shader updates

--- doktorw/renderers/webglRenderer.js
+++ doktorw/renderers/webglRenderer.js
@@ -72,6 +72,32 @@ const RECTANGLE_FRAGMENT_SHADER_SOURCE = `
     }
 `;
 
+const IMAGE_VERTEX_SHADER_SOURCE = `
+    attribute vec2 a_position;
+    attribute vec2 a_texCoord;
+    uniform vec2 u_resolution;
+    varying vec2 v_texCoord;
+
+    void main() {
+        vec2 zeroToOne = a_position / u_resolution;
+        vec2 zeroToTwo = zeroToOne * 2.0;
+        vec2 clipSpace = zeroToTwo - 1.0;
+
+        gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+        v_texCoord = a_texCoord;
+    }
+`;
+
+const IMAGE_FRAGMENT_SHADER_SOURCE = `
+    precision mediump float;
+    uniform sampler2D u_image;
+    varying vec2 v_texCoord;
+
+    void main() {
+        gl_FragColor = texture2D(u_image, v_texCoord);
+    }
+`;
+
 function compileShader(gl, type, source) {
     const shader = gl.createShader(type);
 
